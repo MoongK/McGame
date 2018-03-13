cc219e58456cac16b3b33c53fe0262dcbd4cb29c
MoongK <qorfyd01@gmail.com>
2018-03-13 19:16:44 +0900

    mcgame1

---
+++ Assets/BlockState.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockState : MonoBehaviour {
+
+    public Texture[] Cracks;
+    public ParticleSystem Ef;
+    int Statecounter;
+    //float hardness;
+    //bool isCracked = false;
+
+	//void Start ()
+ //   {
+ //       Statecounter = 0;
+ //       if (gameObject.name == "StoneBlock")
+ //           hardness = 1f;
+ //       else if (gameObject.name == "DirtBlock")
+ //           hardness = 0.5f;
+	//}
+
+    //public void CrCracking()
+    //{
+    //    if(Input.GetMouseButton(0))
+    //    if (!isCracked)
+    //    {
+    //        StartCoroutine(CrCrack());
+    //    }
+    //}
+
+    //IEnumerator CrCrack()
+    //{
+    //    isCracked = true;
+    //    while (isCracked)
+    //    {
+    //        yield return new WaitForSeconds(hardness);
+    //        Statecounter++;
+    //        transform.GetComponent<Renderer>().material.SetTexture("_DetailMask", Cracks[Statecounter]);
+    //    }
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void Cracking()
+    {
+        Invoke("cracker", 0f);
+        CancelInvoke("generator");
+    }
+
+    void cracker()
+    {
+        Statecounter++;
+
+        if (Statecounter == Cracks.Length)
+            Destroy(gameObject);
+        else
+            transform.GetComponent<Renderer>().material.SetTexture("_DetailMask", Cracks[Statecounter]);
+    }
+
+    public void Regeneration()
+    {
+        Invoke("generator", 0.3f);
+    }
+
+    void generator()
+    {
+        Statecounter = 0;
+        transform.GetComponent<Renderer>().material.SetTexture("_DetailMask", Cracks[Statecounter]);
+    }
+
+
+}
---
+++ Assets/InteractionBlock.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionBlock : MonoBehaviour {
+
+    public Object block;
+    public int damagePow;
+    int blockmask;
+    GameObject tempCrackingBlock, currentCrackingBlock;
+
+    private void Start()
+    {
+        damagePow = 10;
+        blockmask = 1 << LayerMask.NameToLayer("Block");
+    }
+
+    void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 4f))
+        {
+            //tempCrackingBlock = hit.collider.gameObject;
+            if (hit.collider.CompareTag("Block"))
+            {
+                //    currentCrackingBlock = hit.collider.gameObject;
+                //    if(currentCrackingBlock == tempCrackingBlock)
+                //        currentCrackingBlock.GetComponent<BlockState>().CrCracking();
+                //    else
+                //        tempCrackingBlock
+
+                if (Input.GetMouseButton(0))
+                hit.transform.gameObject.GetComponent<BlockState>().Cracking();
+            else
+                hit.transform.gameObject.GetComponent<BlockState>().Regeneration();
+        }
+        }
+
+
+        if (Physics.Raycast(ray, out hit, 4f, blockmask))
+        {
+            if (Input.GetMouseButtonDown(1))
+                Instantiate(block, hit.transform.position + hit.normal, Quaternion.identity);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+            block = Resources.Load("DirtBlock");
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+            block = Resources.Load("StoneBlock");
+
+    }
+}
